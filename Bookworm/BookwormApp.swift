2904dc2966f197e5b7b922cf7397f93eef42e94e
Simran Preet Singh <simran5590@gmail.com>
2022-06-12 16:27:11 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Simran Preet Narang on 2022-06-12.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
