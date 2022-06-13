33a0b523aa31e4a61347890750dcf0e31e516e89
Simran Preet Singh <simran5590@gmail.com>
2022-06-12 20:32:37 -0700

    Built a Custom Rating View

--- Bookworm/AddBookView.swift
+++ Bookworm/AddBookView.swift
@@ -36,11 +36,12 @@ struct AddBookView: View {
                 
                 Section {
                     TextEditor(text: $review)
-                    Picker("Rating", selection: $rating) {
-                        ForEach(0..<6) {
-                            Text(String($0))
-                        }
-                    }
+//                    Picker("Rating", selection: $rating) {
+//                        ForEach(0..<6) {
+//                            Text(String($0))
+//                        }
+//                    }
+                    RatingView(rating: $rating)
                 } header: {
                     Text("Write a review")
                 }
